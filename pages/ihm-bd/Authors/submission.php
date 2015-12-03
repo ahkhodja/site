2976ae2fb01abca207fcac605e660438a696843c
ahkhodja <ad.khodja@gmail.com>
2015-12-03 11:47:19 +0100

    nouveau systeme de soumission

--- pages/ihm-bd/Authors/submission.php
+++ pages/ihm-bd/Authors/submission.php
@@ -1,8 +1,8 @@
 <?php session_start();
-if(!isset($_SESSION['id']))
-{
-	header('Location: ../../login.html');
-}
+//if(!isset($_SESSION['id']))
+//{
+//	header('Location: ../../login.html');
+//}
 ?>
 
 <html>
