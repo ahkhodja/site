9b5d93b6531f09fba4cb9d472e9a9546ff597523
ahkhodja <ad.khodja@gmail.com>
2016-01-13 15:10:48 +0100

    nouveau systeme de soumission

--- pages/ihm-bd/Authors/js/submission.js
+++ pages/ihm-bd/Authors/js/submission.js
@@ -76,7 +76,7 @@ $(document).ready(function() {
         $('#abstract_article').text($('#abstract').val());
             $("#co_auteur").html('');
         for(var i=nbco;i>=1;i--) {
-            $("#co_auteur").after(" <div class=\"row\"><p class=\"text-center\">Co-auteur " + i + "</p> </div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">First Name : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_fn" + i + "\" > </label></div></div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">Middle Name : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_mn" + i + "\" > </label></div> </div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">Last Name : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_ln" + i + "\" > </label> </div></div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">Affiliation : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_af" + i + "\" > </label></div></div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">Adresse : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_ad" + i + "\" > </label></div> </div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">E-mail : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_add" + i + "\" > </label></div></div></div>");
+            $("#co_auteur").after(" <div id=\"co_"+i+"\"> <div class=\"row\"><p class=\"text-center\">Co-auteur " + i + "</p> </div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">First Name : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_fn" + i + "\" > </label></div></div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">Middle Name : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_mn" + i + "\" > </label></div> </div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">Last Name : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_ln" + i + "\" > </label> </div></div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">Affiliation : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_af" + i + "\" > </label></div></div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">Adresse : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_ad" + i + "\" > </label></div> </div></div><div class=\"row\"><div class=\"form-group\"><label for=\"text\" class=\"col-lg-2 control-label\">E-mail : </label><div class=\"col-lg-8\"><label for=\"text\" class=\" control-label text-left texte\"id=\"cof_add" + i + "\" > </label></div></div></div></div>");
         }
         return false;}
 
@@ -105,6 +105,8 @@ $(document).ready(function() {
         previous_div.show();
         current_div.hide();
         easing: 'easeInOutBack'
+        for(var i=nbco;i>=1;i--) {
+            $("#co_"+i).remove();      }
         return false;
 
     });
@@ -200,7 +202,7 @@ $(document).ready(function() {
 
         if (res){
             $(".table_image").show();
-            $(".table_image tbody").append(" <tr><td  class=\"text-center\">"+input.val().replace(/\\/g, '/').replace(/.*\//, '')+"</td><td class=\"text-center\">"+getExtension(input.val()).toUpperCase()+"</td><td class=\"text-center\">"+(imgInput.files[0].size/1024).toFixed(2)+' ko'+"</td><td><div class=\"progress bar\"><div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"70\"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:0%\" id=\"img_progresse"+numimg+"\">0%</div></div></td><td><button type=\"button\" class=\"btn btn-danger btn_smal\" id=\"delete\"><i class =\"fa fa-trash-o fa-1x \"></i></button></td></tr>");
+            $(".table_image tbody").append(" <tr><td  class=\"text-center\">"+input.val().replace(/\\/g, '/').replace(/.*\//, '')+"</td><td class=\"text-center\">"+getExtension(input.val()).toUpperCase()+"</td><td class=\"text-center\">"+(imgInput.files[0].size/1024).toFixed(2)+' ko'+"</td><td><div class=\"progress bar\"><div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"70\"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:0%\" id=\"img_progresse"+numimg+"\">0%</div></div></td><td><button type=\"button\" class=\"btn btn-danger btn_smal delete_im\" id=\"delete_im\"><i class =\"fa fa-trash-o fa-1x \"></i></button></td></tr>");
             $('#img_title').html(input.val().replace(/\\/g, '/').replace(/.*\//, '')) ;
             $("#img_taille").html((imgInput.files[0].size/1024).toFixed(2)+' ko');
             $("#img_type").html(getExtension(input.val()).toUpperCase());
@@ -226,6 +228,14 @@ $(document).ready(function() {
             request.upload.addEventListener('load',function(event){
                 console.log('ok');
                 uploaded=true;
+                $(".delete_im").on('click',function(){
+                    var courant=$(this).parent();
+                    var index = $(".block tr").index(courant.parent());
+                    $(".block tr:nth-child("+index+")").hide( "slow" );
+                    //$(".block tr:nth-child("+index+")").remove();
+                   // alert(index);
+
+                });
 
             });
             request.upload.addEventListener('error',function(event){
@@ -240,8 +250,9 @@ $(document).ready(function() {
         alert('type de fichier invalide');
     }
     });
-    $("#delete").click(function(){
-
+    $("#delete_im").click(function(){
+            alert($(this).index());
     });
+
 
 });
